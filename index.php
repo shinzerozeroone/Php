4be5c3ba667aac8c80a9f904aab67179065f1c76
young.shark8484@gmail.com <young.shark8484@gmail.com>
2023-02-19 20:07:04 +0300

    Header
    Добавлен логотип-ссылка вуза и название работы по центру

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php Hello World</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <a href="https://mospolytech.ru/">
+                <img class="header__logo" src="images/0b6dd8bb96f9fc0544405b04019e34b7.png">
+            </a>
+            <p class="header__desc">2.1.Домашняя работа: Hello, World! </p>
+
+        </div>
+    </header>
+</body>
+
+</html>
